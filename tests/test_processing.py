a27c52c9d18fff1c70e211166845700cd214a866
maria merinova <mawenka89@gmail.com>
2024-12-05 22:45:33 +0300

    исправлены тесты

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -3,26 +3,27 @@ import pytest
 from src.processing import filter_by_state, sort_by_date
 
 
-def test_filter_by_state_default_value(test_dict_list):
-    assert filter_by_state(test_dict_list) == [
+@pytest.fixture
+def entered_list():
+    return [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    ]
-
-
-def test_filter_by_state(test_dict_list):
-    assert filter_by_state(test_dict_list, "CANCELED") == [
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
 
 
-def test_filter_by_state_without_requested_state(test_dict_list):
-    assert filter_by_state(test_dict_list, "AUTHORIZATION") == []
+@pytest.fixture
+def executed_list():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
 
 
-def test_sort_by_date(test_dict_list):
-    assert sort_by_date(test_dict_list) == [
+@pytest.fixture
+def date_list():
+    return [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
@@ -30,22 +31,9 @@ def test_sort_by_date(test_dict_list):
     ]
 
 
-def test_sort_by_date_reverse(test_dict_list):
-    assert sort_by_date(test_dict_list, False) == [
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    ]
+def test_filter_by_state(entered_list, executed_list):
+    assert filter_by_state(entered_list) == executed_list
 
 
-def test_sort_by_date_incorrect_formating(test_dict_list_incorrect_date):
-    with pytest.raises(ValueError):
-        sort_by_date(test_dict_list_incorrect_date) == "Invalid isoformat string: '30.06.2018'"
-
-
-def test_sort_by_date_incorrect_formating_texting(test_dict_list_incorrect_date_second_version):
-    with pytest.raises(ValueError):
-        sort_by_date(test_dict_list_incorrect_date_second_version) == (
-            "Invalid isoformat string: 'четырнадцатое октября две" "тысячи восемнадцатого года'"
-        )
+def test_sort_by_date(entered_list, date_list):
+    assert sort_by_date(entered_list) == date_list
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -4,52 +4,26 @@ from src.widget import get_date, mask_account_card
 
 
 @pytest.mark.parametrize(
-    "value, expected",
+    "entered, obtained",
     [
-        ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
-        ("Mastercard 1234567890123456", "Mastercard 1234 56** **** 3456"),
-        ("Мир 2200123456789010", "Мир 2200 12** **** 9010"),
-        ("Union Pay 6010203040506070", "Union Pay 6010 20** **** 6070"),
-        ("Счет 73654108430135874305", "Счет **4305"),
-        ("7000792289606361", "7000 79** **** 6361"),
-        ("73654108430135874305", "**4305"),
+        ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+        ("Счет 64686473678894779589", "Счет **9589"),
+        ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
+        ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
     ],
 )
-def test_mask_account_card(value, expected):
-    assert mask_account_card(value) == expected
+def tests_mask_account_card(entered: str, obtained: str) -> None:
+    """Тест проверяет возвращает ли функция замаскированный номер карты или счета"""
+    assert mask_account_card(entered) == obtained
 
 
 @pytest.mark.parametrize(
-    "value, expected",
+    "old_date, new_date",
     [
-        ("Visa Platinum ", "номер карты не указан"),
-        ("Счет ", "номер счета не указан"),
-        ("", "нет данных"),
-        ("1", "некорректный номер карты"),
+        ("2024-03-11T02:26:18.671407", "11.03.2024"),
+        ("2024-07-01T02:26:18.671407", "01.07.2024"),
     ],
 )
-def test_mask_account_card_valueerror(value, expected):
-    with pytest.raises(ValueError):
-        mask_account_card(value) == expected
-
-
-@pytest.mark.parametrize(
-    "value, expected", [("2024-03-11T02:26:18.671407", "11.03.2024"), ("2024-08-06", "06.08.2024")]
-)
-def test_get_date(value, expected):
-    assert get_date(value) == expected
-
-
-@pytest.mark.parametrize(
-    "value, expected",
-    [
-        ("20/12/2023", "некорректный формат даты"),
-        ("06.05.2021", "некорректный формат даты"),
-        ("двадцать второе июня сорок первого года", "некорректный формат даты"),
-        ("aa-bb-cc", "некорректный формат даты"),
-        ("", "некорректный формат даты"),
-    ],
-)
-def test_get_date_valueerror(value, expected):
-    with pytest.raises(ValueError):
-        get_date(value) == expected
+def tests_get_date(old_date: str, new_date: str) -> None:
+    """Тест проверяет преобразование формата даты"""
+    assert get_date(old_date) == new_date
